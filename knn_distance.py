e8a2888a4f19d7a16ff80e457a60919d82e02913
jot002 <81881824+jot002@users.noreply.github.com>
2022-01-31 22:41:31 -0800

    Create knn_distance.py

---
+++ knn_distance.py
@@ -0,0 +1,11 @@
+import numpy as np
+def knn_distance(arr,q,k):
+    n = len(arr)
+    difference = []
+    orig_index = 0
+    for num in arr:
+        difference.append([np.abs(num-q), orig_index])
+        orig_index += 1
+    difference = sorted(difference)
+
+    return (difference[k-1][0], arr[difference[k-1][1]])
