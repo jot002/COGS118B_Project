770664a1c837b2d8c636bf5a1c72159aabb0f7ba
jot002 <81881824+jot002@users.noreply.github.com>
2022-02-02 23:18:10 -0800

    Update knn_distance.py

--- knn_distance.py
+++ knn_distance.py
@@ -1,3 +1,46 @@
+import random
+
+def partition(arr, start, stop, pivot_ix):
+    
+    left = []
+    pivot_count = 0
+    right = []
+    pivot = arr[pivot_ix]
+    for ix in range(start, stop):
+        if arr[ix] < pivot:
+            left.append(arr[ix])
+        elif arr[ix] == pivot:
+            pivot_count += 1
+        else:
+            right.append(arr[ix])
+    ix = start
+    for x in left:
+        arr[ix] = x
+        ix += 1
+    for i in range(pivot_count):
+        arr[ix] = pivot
+        ix += 1
+    for x in right:
+        arr[ix] = x
+        ix += 1
+    return start + len(left)
+
+def quickselect(arr, k, start, stop):
+    
+    pivot_idx = random.randrange(start, stop)
+    pivot_idx = partition(arr, start, stop, pivot_idx)
+    pivot_order = pivot_idx + 1
+    
+    if pivot_order == k:
+        return arr[pivot_idx]
+    elif pivot_order < k:
+        return quickselect(arr, k, pivot_idx + 1, stop)
+    else:
+        return quickselect(arr, k, 0, pivot_idx)
+
+def absolute(value):
+    return (value**2)**(0.5)
+
 def knn_distance(arr,q,k):
     n = len(arr)
     difference = []
@@ -5,6 +48,7 @@ def knn_distance(arr,q,k):
     for num in arr:
         difference.append([abs(num-q), orig_index])
         orig_index += 1
-    difference = sorted(difference)
-
-    return (difference[k-1][0], arr[difference[k-1][1]])
+    kth_info = quickselect(difference, k, 0, len(difference))
+    distance = kth_info[0]
+    kth_point = arr[kth_info[1]]
+    return (distance, kth_point)
